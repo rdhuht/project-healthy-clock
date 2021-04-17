f10144af7625762c132c7b88b19a5412bcee749f
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2021-04-17 15:33:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -28,21 +28,7 @@
  * 165
  */
 function thingsAndServo () {
-    timeanddate.numericTime(function (hour, minute, second, month, day, year) {
-        if (hour == 6 && minute == 30) {
-            pins.servoWritePin(AnalogPin.P15, 15)
-        } else if (hour == 7 && minute == 0) {
-            pins.servoWritePin(AnalogPin.P15, 45)
-        } else if (hour == 8 && minute == 0) {
-            pins.servoWritePin(AnalogPin.P15, 75)
-        } else if (hour == 12 && minute == 0) {
-            pins.servoWritePin(AnalogPin.P15, 105)
-        } else if (hour == 18 && minute == 0) {
-            pins.servoWritePin(AnalogPin.P15, 135)
-        } else if (hour == 21 && minute == 0) {
-            pins.servoWritePin(AnalogPin.P15, 165)
-        }
-    })
+	
 }
 function blink () {
     if (timeanddate.secondsSinceReset() % 2 == 0) {
@@ -85,14 +71,17 @@ DigitalPin.P2,
 7,
 4
 )
+pins.servoWritePin(AnalogPin.P15, 0)
 timeanddate.set24HourTime(12, 0, 0)
 basic.forever(function () {
     adjustTM()
     blink()
-    thingsAndServo()
 })
 basic.forever(function () {
     timeanddate.numericTime(function (hour, minute, second, month, day, year) {
         tm.showNumber(hour * 100 + minute)
     })
+})
+basic.forever(function () {
+	
 })
